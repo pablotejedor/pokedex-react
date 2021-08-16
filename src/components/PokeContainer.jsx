67a8e0b5c1f11d435306f4cc2cb18478e072c7a3
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-08-16 20:23:34 -0300

    loading added

--- src/components/PokeContainer.jsx
+++ src/components/PokeContainer.jsx
@@ -3,6 +3,7 @@ import { Pokemon } from './Pokemon';
 import { getPokemons } from '../helpers/getPokemons';
 export const PokeContainer = ({ rangeOfPokemons }) => {
   const [pokemons, setPokemons] = useState([]);
+  const [loading, setloading] = useState(true);
   useEffect(() => {
     getPokemons(rangeOfPokemons).then(poke => {
       setPokemons(
@@ -11,15 +12,24 @@ export const PokeContainer = ({ rangeOfPokemons }) => {
             img: poke.sprites.front_default,
             name: poke.name,
             id: poke.id,
-            type: poke.types.map(types => types.type.name[0].toUpperCase() + types.type.name.slice(1)).join(", "),
-            types: poke.types.map(types => types.type.name)
+            type: poke.types
+              .map(
+                types =>
+                  types.type.name[0].toUpperCase() + types.type.name.slice(1)
+              )
+              .join(', '),
+            types: poke.types.map(types => types.type.name),
           };
         })
       );
     });
   }, [rangeOfPokemons]);
+console.log(pokemons)
+  useEffect(() => setloading(false), [pokemons]);
+
   return (
     <div className="poke-container">
+      {loading ? <p>Loading...</p> : null}
       {pokemons.map(poke => (
         <Pokemon pokemon={poke} key={poke.id} />
       ))}
