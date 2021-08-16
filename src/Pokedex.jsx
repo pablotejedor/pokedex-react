5971233528b4bc10d46671acc50f9fce0269fc25
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-08-16 14:53:38 -0300

    generations selector feature deleted, pagination feature implemented instead

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -2,22 +2,41 @@ import React, { useState } from 'react';
 import { PokeContainer } from './components/PokeContainer';
 
 export const Pokedex = () => {
-  const [rangeOfPokemons, setRangeOfPokemons] = useState([1, 151]);
+  const [rangeOfPokemons, setRangeOfPokemons] = useState([1, 30]);
+  const rangeVariation = 30;
+
+  const nextHandler = () => {
+    if (rangeOfPokemons[1] + rangeVariation > 891) {
+      setRangeOfPokemons([
+        (rangeOfPokemons[0] = 870),
+        rangeOfPokemons[1] + (891 - rangeOfPokemons[1]),
+      ]);
+    } else {
+      setRangeOfPokemons(rangeOfPokemons.map(value => value + rangeVariation));
+    }
+  };
+
+  const previousHandler = () => {
+    if (rangeOfPokemons[0] < 30) {
+      return;
+    } else {
+      setRangeOfPokemons(rangeOfPokemons.map(value => value - rangeVariation));
+    }
+  };
+
   return (
     <>
       <h1>Pokedex</h1>
-      <h3>Which generation would you like to see?</h3>
-      <div className="generations-container">
-        <button onClick={() => setRangeOfPokemons([1, 151])}>1st Gen</button>
-        <button onClick={() => setRangeOfPokemons([152, 251])}>2nd Gen</button>
-        <button onClick={() => setRangeOfPokemons([252, 386])}>3rd Gen</button>
-        <button onClick={() => setRangeOfPokemons([387, 494])}>4th Gen</button>
-        <button onClick={() => setRangeOfPokemons([495, 649])}>5th Gen</button>
-        <button onClick={() => setRangeOfPokemons([650, 721])}>6th Gen</button>
-        <button onClick={() => setRangeOfPokemons([722, 809])}>7th Gen</button>
-        <button onClick={() => setRangeOfPokemons([810, 891])}>8th Gen</button>
+      <h3>Gotta catch' em all!</h3>
+      <div className="buttons-container">
+        <button onClick={previousHandler}>Previous</button>
+        <button onClick={nextHandler}>Next</button>
       </div>
       <PokeContainer rangeOfPokemons={rangeOfPokemons} />
+      <div className="buttons-container">
+        <button onClick={previousHandler}>Previous</button>
+        <button onClick={nextHandler}>Next</button>
+      </div>
     </>
   );
 };
