a4628a58739bc2cac69bd9ac611aabbefbe8f3ce
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-08-18 21:44:10 -0300

    performance related changes

--- src/components/PokeContainer.jsx
+++ src/components/PokeContainer.jsx
@@ -6,33 +6,24 @@ export const PokeContainer = ({ rangeOfPokemons }) => {
   const [loading, setloading] = useState(true);
   useEffect(() => {
     getPokemons(rangeOfPokemons).then(poke => {
-      setPokemons(
-        poke.map(poke => {
-          return {
-            img: poke.sprites.front_default,
-            name: poke.name,
-            id: poke.id,
-            type: poke.types
-              .map(
-                types =>
-                  types.type.name[0].toUpperCase() + types.type.name.slice(1)
-              )
-              .join(', '),
-            types: poke.types.map(types => types.type.name),
-          };
-        })
-      );
+      setPokemons(poke);
+      setloading(false);
     });
   }, [rangeOfPokemons]);
-console.log(pokemons)
-  useEffect(() => setloading(false), [pokemons]);
+
+  useEffect(() => {
+    setloading(true);
+  }, [rangeOfPokemons]);
 
   return (
-    <div className="poke-container">
-      {loading ? <p>Loading...</p> : null}
-      {pokemons.map(poke => (
-        <Pokemon pokemon={poke} key={poke.id} />
-      ))}
-    </div>
+    <>
+      {loading ? <p>Loading...</p> :
+      <div className="poke-container">
+        {pokemons.map(poke => (
+          <Pokemon pokemon={poke} key={poke.id} />
+        ))}
+      </div>
+        }
+    </>
   );
 };
--- src/helpers/getPokemons.jsx
+++ src/helpers/getPokemons.jsx
@@ -1,10 +1,53 @@
 export const getPokemons = async rangeOfPokemons => {
-  let pokemons = [];
-  for (let i = rangeOfPokemons[0]; i <= rangeOfPokemons[1]; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+  const fetchPokemons = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     const result = await fetch(url);
     const pokemon = await result.json();
-    pokemons.push(pokemon);
+    return pokemon;
+  };
+
+  const promises = [];
+  for (let i = rangeOfPokemons[0]; i <= rangeOfPokemons[1]; i++) {
+    promises.push(fetchPokemons(i));
   }
-  return pokemons;
+  let pokemons = await Promise.all(promises);
+  let mappedPokemons = pokemons.map(poke => {
+    return {
+      img: poke.sprites.front_default,
+      name: poke.name,
+      id: poke.id,
+      type: poke.types
+        .map(
+          types =>
+            types.type.name[0].toUpperCase() + types.type.name.slice(1)
+        )
+        .join(', '),
+      types: poke.types.map(types => types.type.name),
+    };
+  });
+
+  return mappedPokemons
+;
+
+  // let pokemons = [];
+  // for (let i = rangeOfPokemons[0]; i <= rangeOfPokemons[1]; i++) {
+  //   const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+  //   const result = await fetch(url);
+  //   const pokemon = await result.json();
+  //   pokemons.push(pokemon);
+  // }
+  // return pokemons.map(poke => {
+  //   return {
+  //     img: poke.sprites.front_default,
+  //     name: poke.name,
+  //     id: poke.id,
+  //     type: poke.types
+  //       .map(
+  //         types =>
+  //           types.type.name[0].toUpperCase() + types.type.name.slice(1)
+  //       )
+  //       .join(', '),
+  //     types: poke.types.map(types => types.type.name),
+  //   };
+  // });
 };
